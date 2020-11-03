90f92631893073f6a15ce4ab712c2abab26abcdf
Masaki Hara <ackie.h.gmai@gmail.com>
2020-11-03 18:42:35 +0900

    Adopt to gotham 0.5.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,22 @@
-use futures01::{Future, Poll};
 use gotham::handler::HandlerFuture;
 use gotham::middleware::Middleware;
 use gotham::state::{FromState, State};
 use gotham_derive::NewMiddleware;
-use hyper::HeaderMap;
+use http::header::HeaderMap;
+use pin_project::pin_project;
 use std::collections::HashMap;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
 
 use mightybadger::payload::RequestInfo;
 
 #[derive(Clone, NewMiddleware)]
 pub struct HoneybadgerMiddleware;
 
+#[pin_project]
 struct WithRequestContext<F> {
+    #[pin]
     inner: F,
     context: RequestInfo,
 }
@@ -23,19 +28,19 @@ impl<F> WithRequestContext<F> {
 }
 
 impl<F: Future> Future for WithRequestContext<F> {
-    type Item = F::Item;
-    type Error = F::Error;
+    type Output = F::Output;
 
-    fn poll(&mut self) -> Poll<F::Item, F::Error> {
-        let inner = &mut self.inner;
-        mightybadger::context::with(&self.context, || inner.poll())
+    fn poll(self: Pin<&mut Self>, ctx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+        let inner = this.inner;
+        mightybadger::context::with(&this.context, || inner.poll(ctx))
     }
 }
 
 impl Middleware for HoneybadgerMiddleware {
-    fn call<Chain>(self, state: State, chain: Chain) -> Box<HandlerFuture>
+    fn call<Chain>(self, state: State, chain: Chain) -> Pin<Box<HandlerFuture>>
     where
-        Chain: FnOnce(State) -> Box<HandlerFuture>,
+        Chain: FnOnce(State) -> Pin<Box<HandlerFuture>>,
     {
         let request_info = {
             let mut cgi_data = HashMap::new();
@@ -61,6 +66,6 @@ impl Middleware for HoneybadgerMiddleware {
         };
         let f = mightybadger::context::with(&request_info, || chain(state));
         let f = WithRequestContext::new(f, request_info);
-        Box::new(f)
+        Box::pin(f)
     }
 }
